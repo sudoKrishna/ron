45183ad16be9d55ceb8953c9fbabbdd008b841a5
Krishna <krishnachaudhary2007gt@gmail.com>
2026-05-27 21:50:11 +0530

    fix : fix the typo

--- apps/server/src/services/userService.ts
+++ apps/server/src/services/userService.ts
@@ -2,14 +2,14 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { prisma } from "@repo/db";
 
-const JWT_SECRET = " this";
+const JWT_SECRET = "this";
 
 export const createUser = async(username  : string, password : string) => {
     const existingUser = await prisma.user.findUnique({
         where : {username}
     });
     if(existingUser) {
-        return new Error("user already exists backcodimatker lude")
+        throw new Error("user already exists backcodimatker lude")
     }
     const hashPassword = await bcrypt.hash(password , 10);
 
@@ -20,7 +20,7 @@ export const createUser = async(username  : string, password : string) => {
 
             collateral :  {
                 create : {
-                    available : 0,
+                    available : 100000,
                     locked : 0
                 },
             },
