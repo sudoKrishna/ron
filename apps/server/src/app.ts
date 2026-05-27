7e141a485389795b998f95b0ae3547ab8b6ab943
Krishna <krishnachaudhary2007gt@gmail.com>
2026-05-27 21:44:47 +0530

    adding dotenv add routes

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,19 +1,30 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
+
+import authRoutes from "./routes/auth";
+import marketRoutes from "./routes/marketRoutes";
+import orderRoutes from "./routes/order";
+
+import { startMarketDataService } from "./services/marketDataService";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({
-    extended : true,
-})
-);
+app.use("/auth", authRoutes);
+app.use("/market", marketRoutes);
+app.use("/orders", orderRoutes);
+
 app.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message : "Exchnage API running",
-    });
+  res.json({
+    success: true,
+    message: "Exchange API running",
+  });
 });
+
+startMarketDataService();
 
 export default app;
